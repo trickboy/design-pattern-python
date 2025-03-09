130160b25d448b8b4771111441e74c1f5b7c0367
Patrick Doldolea <pdoldolea@traxiontech.net>
2025-03-09 19:51:02 +0800

    Add proxy.py

--- proxy.py
+++ proxy.py
@@ -24,23 +24,6 @@ class CollegeProxy:
             # Otherwise, don't instantiate the college object.
             print("Your fee balance is greater than 500, first pay the fee")
 
-# """main method"""
-#
-# if __name__ == "__main__":
-#
-#     # Instantiate the Proxy
-#     collegeProxy = CollegeProxy()
-#
-#     # Client attempting to study in the college at the default balance of 1000.
-#     # Logically, since he / she cannot study with such balance,
-#     # there is no need to make the college object.
-#     collegeProxy.studyInCollege()
-#
-#     # Altering the balance of the student
-#     collegeProxy.feeBalance = 100
-#
-#     # Client attempting to study in college at the balance of 100. Should succeed.
-#     collegeProxy.studyInCollege()
 
 # Usage
 collegeProxy = CollegeProxy()
